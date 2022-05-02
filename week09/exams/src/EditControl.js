063aef251794164362ca56b732204829372a9b9c
Fulvio Corno <fulvio.corno@polito.it>
2022-05-02 22:35:36 +0200

    copy of 'exams' project

---
+++ week09/exams/src/EditControl.js
@@ -0,0 +1,13 @@
+import { Button } from "react-bootstrap";
+
+function EditControl(props) {
+
+    return <p align='right'>
+        {props.editable ?
+            <Button variant="outline-secondary" onClick={() => { props.setMode('view') }}>View</Button> :
+            <Button variant="outline-secondary" onClick={() => { props.setMode('change') }}>Change</Button>}
+    </p>;
+
+}
+
+export default EditControl;
---
+++ week09/exams/src/ExamRow.js
@@ -0,0 +1,26 @@
+import { Button } from "react-bootstrap";
+
+function ExamRow(props) {
+    return <tr>
+        <ExamData exam={props.exam}/>
+        {props.editable && <ExamActions exam={props.exam} mode={props.mode} removeExam={props.removeExam}  editExam={props.editExam} />}
+    </tr>;
+}
+
+function ExamData(props) {
+    return <>
+        <td>{props.exam.code}</td>
+        <td>{props.exam.name}</td>
+        <td>{props.exam.score}</td>
+        <td>{props.exam.date.format('YYYY-MM-DD')}</td>
+    </>;
+}
+
+function ExamActions(props) {
+    return <td>
+        <Button variant='outline-danger' disabled={props.mode!=='change'} onClick={()=>{props.removeExam(props.exam.code)}}>Delete</Button> &nbsp;
+        <Button variant='outline-warning' disabled={props.mode!=='change'} onClick={()=>{props.editExam(props.exam)}}>Edit</Button>
+    </td> ;
+}
+
+export default ExamRow;
